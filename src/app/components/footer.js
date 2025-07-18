352b7b75334bd57099a086811c869a8a9b5bfe71
mnkoapp <117938260+mnkoapp@users.noreply.github.com>
2025-07-18 14:24:57 -0400

    &apos; 수정

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <Link href="/">
           <p className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <span className="ml-3 text-xl">Dr.Ko's homepage</span>
+            <span className="ml-3 text-xl">Dr.Ko&apos;s homepage</span>
           </p>
         </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
--- src/app/components/header.js
+++ src/app/components/header.js
@@ -19,7 +19,7 @@ export default function Header() {
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
 
-            <span className="ml-3 text-xl">Dr.Ko's Homepage</span>
+            <span className="ml-3 text-xl">Dr.Ko&apos;s Homepage</span>
           </span>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
